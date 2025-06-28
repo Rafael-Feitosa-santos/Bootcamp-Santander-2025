360747f8d53bfa5ac4907f1028fbe6f717682092
Rafael Feitosa <rafaelfeit@hotmail.com>
2025-06-27 21:13:25 -0300

    atualização do codigo

--- 01 - Decoradores, Iteradores e Geradores com Python/desafio/desafio_v2.py
+++ 01 - Decoradores, Iteradores e Geradores com Python/desafio/desafio_v2.py
@@ -213,6 +213,7 @@ def log_transacao(func):
         resultado = func(*args, **kwargs)
         print(f"{datetime.now()}: {func.__name__.upper()}")
         return resultado
+
     return envelope
 
 
